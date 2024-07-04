123fedd8b3f91269512da53abbe17b6e2fd2cf43
Ma7moudxG <132134084+Ma7moudxG@users.noreply.github.com>
2024-07-04 16:14:56 +0300

    fourth commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: "http://localhost:8000"
+        target: "https://mern-todo-xllz.onrender.com"
       }
     }
   }
